8508ed2fdeefd8719d57131128a21ebe7b0a06f9
ItsAlex <me@itsalex.fr>
2021-04-07 00:09:49 +0200

    refactor: prevent delete of node_modules to run tests in backend

--- backend/scripts/test.sh
+++ backend/scripts/test.sh
@@ -3,17 +3,13 @@ set -e
 
 cd "$(dirname "$0")/../"
 
-docker rm -f test-gibbon-pdf
-rm -rf node_modules/
-
 docker run \
     --rm \
-    --name test-gibbon-pdf \
     -e PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true \
     -e PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium-browser \
-    -v "${PWD}:/src/" \
+    -v "${PWD}/src/:/src/src/" \
+    -v "${PWD}/test:/src/test/" \
+    -v "${PWD}/package.json:/src/package.json" \
+    -v "${PWD}/yarn.lock:/src/yarn.lock" \
     node:14.16.0-alpine3.13 \
     sh -c "apk add --no-cache chromium nss freetype freetype-dev harfbuzz ca-certificates ttf-freefont && cd /src/ && yarn && yarn run test"
-
-docker rm -f test-gibbon-pdf
-rm -rf node_modules/
